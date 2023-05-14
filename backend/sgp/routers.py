242ad28b92828a74034e321992b0ec08c60d0c74
Bruno Bacelar <bruno.bacelar94@hotmail.com>
2023-05-14 20:24:51 -0300

    Rotas para criação de projetos e tasks
    Criadas rotas para criar e recuperar projetos, e criar tasks

--- backend/sgp/routers.py
+++ backend/sgp/routers.py
@@ -1,13 +1,19 @@
-from rest_framework.routers import SimpleRouter
-from sgp.viewsets import UserViewSet
+from rest_framework_nested.routers import NestedSimpleRouter, SimpleRouter
+from sgp.viewsets import UserViewSet, ProjectViewSet, TaskViewSet
 from sgp.auth.viewsets import LoginViewSet, RegistrationViewSet, RefreshViewSet
+from django.urls import path, include
 
 routes = SimpleRouter()
 routes.register(r'auth/login', LoginViewSet, basename='auth-login')
 routes.register(r'auth/register', RegistrationViewSet, basename='auth-register')
 routes.register(r'auth/refresh', RefreshViewSet, basename='auth-refresh')
-routes.register(r'users', UserViewSet, basename='user')
+routes.register(r'users', UserViewSet, basename='users')
+routes.register(r'projects', ProjectViewSet, basename='projects')
+
+project_routes = NestedSimpleRouter(routes, r'projects', lookup='project')
+project_routes.register(r'tasks', TaskViewSet, basename='project-tasks')
 
 urlpatterns = [
-    *routes.urls
+    path("", include(routes.urls)),
+    path("", include(project_routes.urls)),
 ]
