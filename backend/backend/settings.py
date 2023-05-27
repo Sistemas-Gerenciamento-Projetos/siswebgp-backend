35310ddfa00562a31ccdb084b1ccbdfc20848392
Eduardo Ferreira <129122517+Sistemas-Gerenciamento-Projetos@users.noreply.github.com>
2023-05-26 23:33:32 -0300

    configurações postgres no railway

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -88,10 +88,16 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# WSGI_APPLICATION = 'railway_django.wsgi.application'
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'VYVN0ECqFjXALMCFTfXC',
+        'HOST': 'containers-us-west-35.railway.app',
+        'PORT': '6029',
     }
 }
 
